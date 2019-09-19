10082e674094639e46f158d544ff4d40492d8b5d
Wajdi FATHALLAH <wfathallah@valuraise.com>
2019-09-19 15:53:09 +0200

    remove idea

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
-REQUIRED_PACKAGES = ['unicodedata']
-
 setup(name='sparkipy',
-      install_requires=REQUIRED_PACKAGES,
-      version='0.0.1',
+      python_requires='>=3.6',
+      version='0.0.4',
+      author="Wajdi FATHALLAH",
+      author_email="wfathallah@valuraise.com",
+      url="https://github.com/Valuraise/sparkipy",
       description='Framework for pyspark jobs',
       test_suite='tests',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+      ],
       packages=find_packages(),
       zip_safe=False)
